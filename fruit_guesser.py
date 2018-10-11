cec45ecf02478edbb9fc17e1294a9f45e28c8a5e
Rick Aguayo <rick.aguayo@gmail.com>
2018-10-10 23:32:09 -0400

    added code for user to guess 3 times

--- fruit_guesser.py
+++ fruit_guesser.py
@@ -1,12 +1,20 @@
-basket = ['orange', 'apple', 'watermelon', 'pineapple', 'strawberry']
-user_fruit_guess = raw_input("Guess a fruit:\n")
-match = False;
-for fruit in basket:
-    if fruit  == user_fruit_guess:
-        match = True
-if match:
-    print("You guessed right!")
-else:
-    print("Sorry, you guessed wrong!");
+import sys
 
+basket = ['orange', 'apple', 'watermelon', 'pineapple', 'strawberry']
+num_guesses = 1
+while num_guesses <= 3:
+    user_fruit_guess = raw_input("Guess a fruit: ")
+    match = False;
+    for fruit in basket:
+        if fruit  == user_fruit_guess:
+            match = True
+    if match:
+        print("You guessed right!")
+        sys.exit(0)
+    else:
+        print("Sorry, you guessed wrong!");
+        if num_guesses < 3:
+            print("Guess again, I'll let you try " + str(3 - num_guesses) + " more time(s).\n")
+        
+    num_guesses += 1
 
