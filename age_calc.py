fa5df4a85a939cdd9ce83bbaeae2e57135100a27
root <root@localhost.localdomain>
2018-10-10 21:29:57 -0400

    first version of basic age calc

---
+++ age_calc.py
@@ -0,0 +1,9 @@
+import datetime;
+
+current_age = int(raw_input("What is your age?\n"));
+#print(type(current_age));
+
+now = datetime.datetime.now();
+years_until_2035 = 2035 - now.year;
+age_in_2035 = current_age + years_until_2035;
+print("You will be " + str(age_in_2035) + " in the year 2035.");
